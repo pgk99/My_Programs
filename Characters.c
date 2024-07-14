5dfb6c21b74251c3b37760d8e83ec04e86d0798b
Pranav Kavishwar <pranavgk99@gmail.com>
2024-07-14 04:28:49 +0000

    C code to print capital letters

---
+++ Characters.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    /*Characters from A to Z, we'll use ACII values....*/
+    for (int i=65;i<=90;i++)
+        {
+            printf("%c ",i);
+        }
+    return 0;
+}
+
