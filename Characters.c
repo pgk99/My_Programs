d9d5d4819f5b1b1187022de69f4947af972b1bfa
Pranav Kavishwar <pranavgk99@gmail.com>
2024-07-17 17:06:02 +0530

    Added code snippet to print small alphabets

--- Characters.c
+++ Characters.c
@@ -2,10 +2,19 @@
 int main()
 {
     /*Characters from A to Z, we'll use ACII values....*/
-    for (int i=65;i<=90;i++)
+    int i;
+
+    for (i = 65;i <= 90; i++)
         {
             printf("%c ",i);
         }
+
+    /*Characters from a to z, we'll use ASCII values*/
+    for (i = 97; i <= 122; i++)
+    {
+        printf("%c ",i);
+    }
+    
     return 0;
 }
 
